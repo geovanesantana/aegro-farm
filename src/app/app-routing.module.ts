e6c7e5e33c73edbd40f24da6dbd914f6c3057bbd
Geovane Santana (Valtech) <geovane.santana@valtech.com>
2021-04-26 09:51:12 -0300

    change modules and styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FarmDetailComponent } from './farms/farm-detail/farm-detail.component';
+import { FarmListComponent } from './farms/farm-list/farm-list.component';
+import { FarmRegisterComponent } from './farms/farm-register/farm-register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FarmListComponent },
+  { path: 'cadastro/:id', component: FarmRegisterComponent },
+  { path: 'fazenda/:id', component: FarmDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FarmListComponent } from './farms/farm-list/farm-list.component';
+import { FarmDetailComponent } from './farms/farm-detail/farm-detail.component';
+import { FarmRegisterComponent } from './farms/farm-register/farm-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FarmListComponent,
+    FarmDetailComponent,
+    FarmRegisterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
